448301440d58d228d19a560ee172aa42c5d107f7
Mamadou Lamine Ba <lamine.ba@thinktech.sn>
2018-01-08 21:21:06 +0000

    improving the project.

--- WebContent/sw.js
+++ WebContent/sw.js
@@ -1,7 +1,8 @@
 const assets = ["/","."];
+const cacheName = "assets-v-1";
 self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open("assets").then(function(cache) {
+    caches.open(cacheName).then(function(cache) {
       return cache.addAll(assets);
     })
   );
@@ -9,7 +10,7 @@ self.addEventListener('install', function(event) {
 self.addEventListener('fetch', function(event) {
 	  const request = event.request;
 	  event.respondWith(
-	    caches.open('assets').then(function(cache) {
+	    caches.open(cacheName).then(function(cache) {
 	      return cache.match(request).then(function (response) {
 	        return response || fetch(request).then(function(response) {
 	          if(request.url.indexOf("/images/")!=-1){
