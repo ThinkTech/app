0d3e5cdf26ae489843b7b89e23667583d2515624
Mamadou Lamine Ba <lamine.ba@thinktech.sn>
2018-01-19 19:40:45 +0000

    improving the project.

--- WebContent/modules/management/js/projects.js
+++ WebContent/modules/management/js/projects.js
@@ -48,7 +48,7 @@ $(document).ready(function(){
 							 $(".info-message").hide();
 							 const info = $(this).parent().prev();
 							 var left = event.pageX-info.width()-50;
-							 left = left < 0 ? 5 : left;
+							 left = left < 0 ? 2 : left;
 							 info.css({top : event.pageY-20,left : left}).show();
 							 return false;
 						});
