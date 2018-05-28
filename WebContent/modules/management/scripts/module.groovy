28c149f009b23f612bea7076ab9eca47f47780a6
Thinktech <dev@thinktech.sn>
2018-05-28 14:03:49 +0000

    improving the project.

--- WebContent/modules/management/scripts/module.groovy
+++ WebContent/modules/management/scripts/module.groovy
@@ -28,40 +28,6 @@ class ModuleAction extends ActionSupport {
        request.setAttribute("bills_count",bills_count)
    	   SUCCESS
    }
-     	
-   def showMessages(){
-	   def connection = getConnection()
-       def messages = []
-       connection.eachRow("select m.id,m.subject,m.message,m.date,m.unread,u.name from messages m, users u where m.structure_id = ? and m.user_id = u.id order by m.date DESC",[user.structure.id], { row -> 
-          def message = new Expando()
-          message.with{
-            id = row.id
-            subject = row.subject
-            date = row.date
-            user = row.name
-            unread = row.unread  
-          }
-		  messages << message
-       })
-       def unread = connection.firstRow("select count(*) AS num from messages where unread = true and structure_id = "+user.structure.id).num
-       connection.close() 
-       request.setAttribute("messages",messages)  
-       request.setAttribute("total",messages.size())
-       request.setAttribute("unread",unread)
-       SUCCESS
-    }
-    
-    def getMessageInfo() {
-	   def id = getParameter("id")
-	   def connection = getConnection()
-	   def message = connection.firstRow("select m.*, u.name from messages m, users u where m.user_id=u.id and m.id = ?", [id])
-	   if(message.subject.length()>40) message.subject = message.subject.substring(0,40)+"..."
-	   message.date = new SimpleDateFormat("dd/MM/yyyy - HH:mm:ss").format(message.date)
-	   connection.executeUpdate 'update messages set unread = false where id = ?', [id] 
-	   connection.close()
-	   response.setHeader("Cache-control", "private, max-age=78840000")
-	   json([entity : message])
-	}
     
 	def showServices(){
 	   request.setAttribute("total",1)
