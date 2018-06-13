d118f63e6c09add87e3875a0ee6a79167b9d8eaf
ThinkTech <dev@thinktech.sn>
2018-06-13 11:32:14 +0000

    improving the project.

--- WebContent/modules/management/js/projects.js
+++ WebContent/modules/management/js/projects.js
@@ -98,36 +98,6 @@ app.ready(function(){
 				}
 			});
 		}
-		$("a.pay",container).click(function(event) {
-			page.details.hide();
-			const top = $(this).offset().top;
-			page.wait({top : top});
-			head.load("modules/payment/js/wizard.js",function() {
-			    payment.wizard.show(project.bill,top,function(){
-			    	const tr = $(".table tr[id="+project.id+"]");
-					$("span.label",tr).html("en cours").removeClass().addClass("label label-danger");
-					$(".badge",tr).html("10%");
-					var h3 = $("h3.unactive");
-					h3.html(parseInt(h3.text())-1);
-					h3 = $("h3.active");
-					h3.html(parseInt(h3.text())+1);
-					h3 = $("h3.unpayed");
-					h3.html(parseInt(h3.text())-1);
-					const wizard = $(".project-wizard");
-					page.render(wizard, project, false, function() {
-						$("> div section:nth-child(1)",wizard).hide();
-						$("> div section:nth-child(2)",wizard).show();
-						$("> div section:nth-child(2) input[type=button]",wizard).click(function(event) {
-							  wizard.hide();
-						}); 
-					});
-					wizard.show();
-			    });
-			});
-		});
-	    if(project.plan == "plan social" || project.plan == "plan custom") {
-	    	$("a.pay",container).hide().prev().hide().prev().hide();
-	    }
 		$("a.tasks",container).click(function(event) {
 			$(".info-tasks",container).toggle();
 		});
