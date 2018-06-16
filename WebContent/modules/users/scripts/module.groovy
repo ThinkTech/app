780cf5d82b0674ad719f85fc59f1a46ae989df7d
ThinkTech <dev@thinktech.sn>
2018-06-16 18:21:56 +0000

    improving the project.

--- WebContent/modules/users/scripts/module.groovy
+++ WebContent/modules/users/scripts/module.groovy
@@ -187,7 +187,7 @@ class ModuleAction extends ActionSupport {
 		 div(style : "font-family:Tahoma;background:#fafafa;padding-bottom:16px;padding-top: 25px"){
 		 div(style : "padding-bottom:12px;margin-left:auto;margin-right:auto;width:80%;background:#fff") {
 		    img(src : "https://www.thinktech.sn/images/logo.png", style : "display:block;margin : 0 auto")
-		    div(style : "margin-top:10px;padding-top:2%;height:100px;text-align:center;background:#05d2ff") {
+		    div(style : "margin-top:10px;padding-bottom:2%;padding-top:2%;text-align:center;background:#05d2ff") {
 		      h4(style : "font-size: 200%;color: #fff;margin: 3px") {
 		        span("R&eacute;initialisation de votre mot de passe")
 		      }
@@ -222,7 +222,7 @@ class ModuleAction extends ActionSupport {
 		 div(style : "font-family:Tahoma;background:#fafafa;padding-bottom:16px;padding-top: 25px"){
 		 div(style : "padding-bottom:12px;margin-left:auto;margin-right:auto;width:80%;background:#fff") {
 		    img(src : "https://www.thinktech.sn/images/logo.png", style : "display:block;margin : 0 auto")
-		    div(style : "margin-top:10px;padding-top:2%;height:100px;text-align:center;background:#05d2ff") {
+		    div(style : "margin-top:10px;padding-bottom:2%;padding-top:2%;text-align:center;background:#05d2ff") {
 		      h4(style : "font-size: 200%;color: #fff;margin: 3px") {
 		        span("Demande de collaboration")
 		      }
