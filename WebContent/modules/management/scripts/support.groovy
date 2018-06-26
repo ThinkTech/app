d4bea09a889d14a9b144eb9bc9d73380d171e2f3
ThinkTech <dev@thinktech.sn>
2018-06-26 13:31:38 +0000

    improving the project.

--- WebContent/modules/management/scripts/support.groovy
+++ WebContent/modules/management/scripts/support.groovy
@@ -35,7 +35,7 @@ class ModuleAction extends ActionSupport {
 	   def connection = getConnection()
 	   def params = [ticket.subject,ticket.service,ticket.message,ticket.priority,user.id,user.structure.id]
        def result = connection.executeInsert 'insert into tickets(subject,service,message,priority,user_id,structure_id) values (?, ?, ?, ?,?,?)', params
-	   sendMail("ThinkTech Support","support@thinktech.sn","${ticket.subject}",getTicketTemplate(user,ticket))
+	   sendMail("ThinkTech Support","support@thinktech.sn","Nouveau Ticket : ${ticket.subject}",getTicketTemplate(user,ticket))
 	   json([id: result[0][0]])
 	   connection.close()
 	}
@@ -67,13 +67,12 @@ class ModuleAction extends ActionSupport {
 	
 	def addTicketComment() {
 	   def comment = parse(request)
-	   def user_id = user.id
-	   Thread.start {
-	     def connection = getConnection()
-	     def params = [comment.message,comment.ticket,user_id]
-         connection.executeInsert 'insert into tickets_comments(message,ticket_id,createdBy) values (?,?,?)', params
-	     connection.close()
-	   } 
+	   def connection = getConnection()
+	   def params = [comment.message,comment.ticket,user.id]
+       connection.executeInsert 'insert into tickets_comments(message,ticket_id,createdBy) values (?,?,?)', params
+       def subject = connection.firstRow("select subject from tickets  where id = ?", [comment.ticket]).subject
+       sendMail("ThinkTech Support","support@thinktech.sn","Ticket : ${subject}",getCommentTemplate(user,comment))
+	   connection.close()
 	   json([status: 1])
 	}
 	
@@ -111,6 +110,36 @@ class ModuleAction extends ActionSupport {
 		template.toString()
 	}
 	
+	def getCommentTemplate(user,comment) {
+		MarkupTemplateEngine engine = new MarkupTemplateEngine()
+		def text = '''\
+		 div(style : "font-family:Tahoma;background:#fafafa;padding-bottom:16px;padding-top: 25px"){
+		 div(style : "padding-bottom:12px;margin-left:auto;margin-right:auto;width:80%;background:#fff") {
+		    img(src : "https://www.thinktech.sn/images/logo.png", style : "display:block;margin : 0 auto")
+		    div(style : "margin-top:10px;padding-bottom:2%;padding-top:2%;text-align:center;background:#05d2ff") {
+		      h4(style : "font-size: 120%;color: #fff;margin: 3px") {
+		        span("Nouveau commentaire cr&eacute;&eacute;")
+		      }
+		    }
+		    div(style : "width:90%;margin:auto;margin-top : 30px;margin-bottom:30px") {
+		     h5(style : "font-size: 90%;color: rgb(0, 0, 0);margin-bottom: 0px") {
+		         span("Auteur : $user.name")
+		     }
+		     p("$comment.message")
+
+		    }
+		    div(style : "text-align:center;margin-top:30px;margin-bottom:10px") {
+			    a(href : "$url/dashboard/support",style : "font-size:130%;width:140px;margin:auto;text-decoration:none;background: #05d2ff;display:block;padding:10px;border-radius:2px;border:1px solid #eee;color:#fff;") {
+			        span("R&eacute;pondre")
+			    }
+			}
+		  }
+		  
+		 }
+		'''
+		def template = engine.createTemplate(text).make([comment:comment,user:user,url : "https://thinktech-crm.herokuapp.com"])
+		template.toString()
+	}
 	
 	def getConnection()  {
 	   new Sql(dataSource)	
