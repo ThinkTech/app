57df7c54cb9b1c6a536dd6180ab5f596bd636ffc
ThinkTech <dev@thinktech.sn>
2018-07-06 10:34:11 +0000

    improving the project.

--- WebContent/modules/users/scripts/module.groovy
+++ WebContent/modules/users/scripts/module.groovy
@@ -3,10 +3,7 @@ class ModuleAction extends ActionSupport {
     def String execute(){
        if(user){
          def connection = getConnection()
-         def collaborators = []
-         connection.eachRow("select u.id, u.name,a.activated as active,a.locked from users u, accounts a where u.structure_id = ? and u.owner = false and a.user_id = u.id", [user.structure.id], { row -> 
-          collaborators << new Expando(row.toRowResult())
-         })
+         def collaborators = connection.rows("select u.id, u.name,a.activated as active,a.locked from users u, accounts a where u.structure_id = ? and u.owner = false and a.user_id = u.id", [user.structure.id])
          connection.close()
          request.setAttribute("collaborators",collaborators)
          SUCCESS
