018fe9998b01011e10152e3c2b008e88053bbcaf
ThinkTech <dev@thinktech.sn>
2018-07-28 22:33:28 +0000

    improving the project.

--- WebContent/modules/management/scripts/projects.groovy
+++ WebContent/modules/management/scripts/projects.groovy
@@ -111,8 +111,11 @@ class ModuleAction extends ActionSupport {
 		      }
 		    }
 		    div(style : "width:90%;margin:auto;margin-top : 30px;margin-bottom:30px") {
-		     h5(style : "font-size: 90%;color: rgb(0, 0, 0);margin-bottom: 0px") {
+		     h5(style : "font-size: 90%;color: rgb(0, 0, 0);margin-top:5px;margin-bottom: 0px") {
 		         span("Auteur : $user.name")
+		     }
+		     h5(style : "font-size: 90%;color: rgb(0, 0, 0);margin-top:5px;margin-bottom: 0px") {
+		         span("Structure : $user.structure.name")
 		     }
 		     p("$comment.message")
 
