84f3b911dd77995b677e352c7c04a87776f38a9e
thinktech <dev@thinktech.sn>
2018-09-12 18:58:57 +0000

    improving the project.

--- WebContent/modules/users/scripts/module.groovy
+++ WebContent/modules/users/scripts/module.groovy
@@ -49,7 +49,7 @@ class ModuleAction extends ActionSupport {
         user.structure = connection.firstRow("select * from structures where id = ?", [user.structure_id])
 	   	connection.executeUpdate 'update users set password = sha(?) where email = ?', [user.password,user.email]
 	   	connection.close()
-	   	sendMail("$user.name","$user.email","Changement de votre mot de passe",getPasswordTemplate(user))
+	   	sendMail(user.name,user.email,"Changement de votre mot de passe",getPasswordTemplate(user))
 	   	json([status: 1])
 	   }else {
 	   	json([status: 0])
@@ -99,7 +99,7 @@ class ModuleAction extends ActionSupport {
           def id = result[0][0]
           params = [user.activationCode,id]
        	  connection.executeInsert 'insert into accounts(activation_code,user_id) values (?, ?)', params
-	      sendMail("$user.email","$user.email","Veuillez confirmer cette demande de collaboration",getCollaborationTemplate(user))
+	      sendMail(user.email,user.email,"Veuillez confirmer cette demande de collaboration",getCollaborationTemplate(user))
 	   	  json([id : id])
  	   }
  	   connection.close()
@@ -114,7 +114,7 @@ class ModuleAction extends ActionSupport {
  		def params = [user.activationCode,user.id]
        	connection.executeUpdate 'update accounts set activated = false,activation_code = ? where user_id = ?', params 
  		connection.close() 
-	    sendMail("$user.email","$user.email","Veuillez confirmer cette demande de collaboration",getCollaborationTemplate(user))
+	    sendMail(user.email,user.email,"Veuillez confirmer cette demande de collaboration",getCollaborationTemplate(user))
 	   	json([status : 1])
 	}
 	
