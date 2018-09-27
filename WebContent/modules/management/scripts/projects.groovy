9258fb74cba0ff4ea72d8c2bbb4e7668b5ca8af6
thinktech <dev@thinktech.sn>
2018-09-27 20:47:25 +0000

    improving the project.

--- WebContent/modules/management/scripts/projects.groovy
+++ WebContent/modules/management/scripts/projects.groovy
@@ -63,7 +63,7 @@ class ModuleAction extends ActionSupport {
 	   def params = [comment.message,comment.project,user.id]
        connection.executeInsert 'insert into projects_comments(message,project_id,createdBy) values (?,?,?)', params
        def subject = connection.firstRow("select subject from projects  where id = ?", [comment.project]).subject
-       sendSupportMail("Projet : ${subject}",getCommentTemplate(user,comment))
+       sendSupportMail("Projet : ${subject}",parseTemplate("project_comment",[comment:comment,user:user,url : "https://thinktech-crm.herokuapp.com"]))
 	   connection.close()
 	   json([status: 1])
 	}
@@ -94,40 +94,6 @@ class ModuleAction extends ActionSupport {
 	   connection.executeUpdate "update projects set description = ? where id = ?", [project.description,project.id] 
 	   connection.close()
 	   json([status: 1])
-	}
-	
-	def getCommentTemplate(user,comment){
-		def text = '''\
-		 div(style : "font-family:Tahoma;background:#fafafa;padding-bottom:16px;padding-top: 25px"){
-		 div(style : "padding-bottom:12px;margin-left:auto;margin-right:auto;width:80%;background:#fff") {
-		    img(src : "https://www.thinktech.sn/images/logo.png", style : "display:block;margin : 0 auto")
-		    div(style : "margin-top:10px;padding-bottom:2%;padding-top:2%;text-align:center;background:#05d2ff") {
-		      h4(style : "font-size: 120%;color: #fff;margin: 3px") {
-		        span("Nouveau commentaire ajout&eacute;")
-		      }
-		    }
-		    div(style : "width:90%;margin:auto;margin-top : 20px;margin-bottom:30px") {
-		     h5(style : "font-size: 90%;color: rgb(0, 0, 0);margin-top:5px;margin-bottom: 0px") {
-		         span("Auteur : $user.name")
-		     }
-		     h5(style : "font-size: 90%;color: rgb(0, 0, 0);margin-top:5px;margin-bottom: 0px") {
-		         span("Structure : $user.structure.name")
-		     }
-		     p("$comment.message")
-
-		    }
-		    div(style : "text-align:center;margin-top:30px;margin-bottom:10px") {
-			    a(href : "$url/dashboard/projects",style : "font-size:130%;width:140px;margin:auto;text-decoration:none;background: #05d2ff;display:block;padding:10px;border-radius:2px;border:1px solid #eee;color:#fff;") {
-			        span("R&eacute;pondre")
-			    }
-			}
-		  }
-		  
-		 }
-		'''
-		MarkupTemplateEngine engine = new MarkupTemplateEngine()
-		def template = engine.createTemplate(text).make([comment:comment,user:user,url : "https://thinktech-crm.herokuapp.com"])
-		template.toString()
 	}
 	
 }
--- WebContent/modules/management/scripts/support.groovy
+++ WebContent/modules/management/scripts/support.groovy
@@ -16,7 +16,7 @@ class ModuleAction extends ActionSupport {
 	   def connection = getConnection()
 	   def params = [ticket.subject,ticket.service,ticket.message,ticket.priority,user.id,user.structure.id]
        def result = connection.executeInsert 'insert into tickets(subject,service,message,priority,user_id,structure_id) values (?, ?, ?, ?,?,?)', params
-	   sendSupportMail("Nouveau Ticket : ${ticket.subject}",getTicketTemplate(user,ticket))
+	   sendSupportMail("Nouveau Ticket : ${ticket.subject}",parseTemplate("ticket",[ticket:ticket,user:user,url : "https://thinktech-crm.herokuapp.com"]))
 	   json([id: result[0][0]])
 	   connection.close()
 	}
@@ -51,75 +51,10 @@ class ModuleAction extends ActionSupport {
 	   def params = [comment.message,comment.ticket,user.id]
        connection.executeInsert 'insert into tickets_comments(message,ticket_id,createdBy) values (?,?,?)', params
        def subject = connection.firstRow("select subject from tickets  where id = ?", [comment.ticket]).subject
-       sendSupportMail("Ticket : ${subject}",getCommentTemplate(user,comment))
+       sendSupportMail("Ticket : ${subject}",parseTemplate("ticket_comment",[comment:comment,user:user,url : "https://thinktech-crm.herokuapp.com"]))
 	   connection.close()
 	   json([status: 1])
 	}
 	
-	def getTicketTemplate(user,ticket){
-		def text = '''\
-		 div(style : "font-family:Tahoma;background:#fafafa;padding-bottom:16px;padding-top: 25px"){
-		 div(style : "padding-bottom:12px;margin-left:auto;margin-right:auto;width:80%;background:#fff") {
-		    img(src : "https://www.thinktech.sn/images/logo.png", style : "display:block;margin : 0 auto")
-		    div(style : "margin-top:10px;padding-bottom:2%;padding-top:2%;text-align:center;background:#05d2ff") {
-		      h4(style : "font-size: 120%;color: #fff;margin: 3px") {
-		        span("Nouveau ticket pour assistance")
-		      }
-		    }
-		    div(style : "width:90%;margin:auto;margin-top : 20px;margin-bottom:30px") {
-		     h5(style : "font-size: 90%;color: rgb(0, 0, 0);margin-top:5px;margin-bottom: 0px") {
-		         span("Auteur : $user.name")
-		     }
-		     h5(style : "font-size: 90%;color: rgb(0, 0, 0);margin-top:5px;margin-bottom: 0px") {
-		         span("Structure : $user.structure.name")
-		     }
-		     p("$ticket.message")
-		    }
-		    div(style : "text-align:center;margin-top:30px;margin-bottom:10px") {
-			    a(href : "$url/dashboard/support",style : "font-size:130%;width:140px;margin:auto;text-decoration:none;background: #05d2ff;display:block;padding:10px;border-radius:2px;border:1px solid #eee;color:#fff;") {
-			        span("Voir")
-			    }
-			}
-		  }
-		  
-		 }
-		'''
-		MarkupTemplateEngine engine = new MarkupTemplateEngine()
-		def template = engine.createTemplate(text).make([ticket:ticket,user:user,url : "https://thinktech-crm.herokuapp.com"])
-		template.toString()
-	}
-	
-	def getCommentTemplate(user,comment){
-		def text = '''\
-		 div(style : "font-family:Tahoma;background:#fafafa;padding-bottom:16px;padding-top: 25px"){
-		 div(style : "padding-bottom:12px;margin-left:auto;margin-right:auto;width:80%;background:#fff") {
-		    img(src : "https://www.thinktech.sn/images/logo.png", style : "display:block;margin : 0 auto")
-		    div(style : "margin-top:10px;padding-bottom:2%;padding-top:2%;text-align:center;background:#05d2ff") {
-		      h4(style : "font-size: 120%;color: #fff;margin: 3px") {
-		        span("Nouveau commentaire ajout&eacute;")
-		      }
-		    }
-		    div(style : "width:90%;margin:auto;margin-top : 20px;margin-bottom:30px") {
-		     h5(style : "font-size: 90%;color: rgb(0, 0, 0);margin-top:5px;margin-bottom: 0px") {
-		         span("Auteur : $user.name")
-		     }
-		     h5(style : "font-size: 90%;color: rgb(0, 0, 0);margin-top:5px;margin-bottom: 0px") {
-		         span("Structure : $user.structure.name")
-		     }
-		     p("$comment.message")
-		    }
-		    div(style : "text-align:center;margin-top:30px;margin-bottom:10px") {
-			    a(href : "$url/dashboard/support",style : "font-size:130%;width:140px;margin:auto;text-decoration:none;background: #05d2ff;display:block;padding:10px;border-radius:2px;border:1px solid #eee;color:#fff;") {
-			        span("R&eacute;pondre")
-			    }
-			}
-		  }
-		  
-		 }
-		'''
-		MarkupTemplateEngine engine = new MarkupTemplateEngine()
-		def template = engine.createTemplate(text).make([comment:comment,user:user,url : "https://thinktech-crm.herokuapp.com"])
-		template.toString()
-	}
 	
 }
