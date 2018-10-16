6799ce1f0baf80c8e3902a29ed22f72bad713953
thinktech <dev@thinktech.sn>
2018-10-16 15:10:17 +0000

    improving the project.

--- src/app/UploadServlet.java
+++ src/app/UploadServlet.java
@@ -25,8 +25,7 @@ public class UploadServlet extends HttpServlet {
 				FileManager manager = new FileManager();
 				while(iter.hasNext()) {
 					FileItemStream item = iter.next(); 
-					String name = new File(item.getName()).getName();
-					manager.upload(directory+"/"+name,item.openStream());
+					manager.upload(directory+"/"+new File(item.getName()).getName(),item.openStream());
 				}
 			}catch(Exception e){
 			}
