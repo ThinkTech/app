a40588edbffab19ead5e70eca04c49f8c08762d1
thinktech <dev@thinktech.sn>
2018-10-17 14:37:53 +0000

    improving the project.

--- src/app/UploadServlet.java
+++ src/app/UploadServlet.java
@@ -20,10 +20,10 @@ public class UploadServlet extends HttpServlet {
 		if(isMultipart){
 			try {
 				String directory = "structure_"+request.getParameter("structure_id")+"/"+"project_"+request.getParameter("project_id");
-				FileItemIterator iter = new ServletFileUpload().getItemIterator(request);
 				FileManager manager = new FileManager();
-				while(iter.hasNext()) {
-					FileItemStream item = iter.next(); 
+				FileItemIterator it = new ServletFileUpload().getItemIterator(request);
+				while(it.hasNext()) {
+					FileItemStream item = it.next(); 
 					manager.upload(directory+"/"+new File(item.getName()).getName(),item.openStream());
 				}
 			}catch(Exception e){
